3e90318abc7d572c69feac727bbd215b48bc8da8
HeartsDo <heartsdo@vivaldi.net>
2020-08-08 17:34:43 +0200

    Update verify-version.py

--- verify-version.py
+++ verify-version.py
@@ -5,6 +5,8 @@ lv = config.lastversion
 
 if os.getenv("lv") != lv:
     config.lastversion = os.getenv("lv")
+    print("New Version found !")
     sys.exit(0)
 else:
+    print("No new Version found...")
     sys.exit(1)
