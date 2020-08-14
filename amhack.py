af3bc4151dcad4e46d5c97548e935f6b2179b48c
HeartsDo <heartsdo@vivaldi.net>
2020-08-14 19:14:36 +0200

    Update amhack.py

--- amhack.py
+++ amhack.py
@@ -4,6 +4,12 @@ import yaml
 version = os.getenv("version")
 path = os.getenv("GITHUB_WORKSPACE")
 
+def meta_constructor(loader, node):
+    value = loader.construct_mapping(node)
+    return value
+
+yaml.add_constructor(u'tag:yaml.org,2002:brut.androlib.meta.MetaInfo', meta_constructor)
+
 with open(path + "/osu/apktool.yml") as f:
      filechange = yaml.load(f, Loader=yaml.FullLoader)
 
